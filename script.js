768c7d9d09ff7e8810aa211589dd2f82a49125ac
waltermatulich <wmatulich231@gmail.com>
2024-01-06 19:53:47 -0800

    updated HTML & Js  added CSS.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 var APIKey = f95281993c521c14c228e4b130992126;
+
+var city;
+
+var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q={Napa},{US}&appid={f95281993c521c14c228e4b130992126}";
+
+fetch(queryUrl);
