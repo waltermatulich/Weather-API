b4ea665c346f47fb5db9558431ee1bcc7f169a45
waltermatulich <wmatulich231@gmail.com>
2024-01-09 15:59:20 -0800

    added changes to HTML, CSS, and Java

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-var APIKey = f95281993c521c14c228e4b130992126;
+const APIKey = "f95281993c521c14c228e4b130992126" ;
 
-var city;
+var city
 
-var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q={Napa},{US}&appid={f95281993c521c14c228e4b130992126}";
+var buttonTarget = document.getElementById("search-button");
 
-fetch(queryUrl);
+async function getWeatherForecast() {
+    var cityTarget = document.getElementById("city-input").value;
+    const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityTarget}&appid=${APIKey}`
+console.log(cityTarget)
+    try {
+        const response = await fetch(APIUrl);
+         console.log(response)
+        const weatherData = await response.json();
+        // Process and use the weather data as needed
+        console.log(weatherData)
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+}
+
+buttonTarget.addEventListener("click", getWeatherForecast);
